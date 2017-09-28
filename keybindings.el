a3efc405c8c103e3a3d783a1a4f1019cc78c1e48
Oshiumi <toshiumi@aiming-inc.com>
2017-09-28 22:59:17 +0900

    add keybinding in multi-term

--- keybindings.el
+++ keybindings.el
@@ -3,3 +3,7 @@
 (keyboard-translate ?\C-x ?\C-t)
 (global-unset-key "\C-j")
 (keyboard-translate ?\C-h ?\C-?)
+
+(define-key term-raw-map (kbd "C-j") nil)
+(define-key term-raw-map (kbd "C-p") 'term-send-up)
+(define-key term-raw-map (kbd "C-n") 'term-send-down)
