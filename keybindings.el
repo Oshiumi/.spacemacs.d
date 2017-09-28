cacfe52428fd1028a92cd6eacec7172a4e8fc5b5
Oshiumi <toshiumi@aiming-inc.com>
2017-09-28 22:40:45 +0900

    C-h

--- keybindings.el
+++ keybindings.el
@@ -1,6 +1,5 @@
 ;; key bindings
 (keyboard-translate ?\C-t ?\C-x)
 (keyboard-translate ?\C-x ?\C-t)
-(global-set-key (kbd "C-h") 'delete-backward-char)
-(define-key term-raw-map (kbd "C-h") 'term-send-backspace)
 (global-unset-key "\C-j")
+(keyboard-translate ?\C-h ?\C-?)
