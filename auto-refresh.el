47d32ecb2287a84e3e8581c01b0b5dc9369a820a
Oshiumi <toshiumi@aiming-inc.com>
2017-12-25 20:21:42 +0900

    add auto-refresh

---
+++ auto-refresh.el
@@ -0,0 +1,10 @@
+(setq auto-revert-check-vc-info t)
+
+(setq auto-revert-interval 1)
+
+(add-hook 'find-file-hook
+          '(lambda ()
+             (when
+                 (and buffer-file-name
+                      (vc-backend buffer-file-name))
+               (auto-revert-mode))))
