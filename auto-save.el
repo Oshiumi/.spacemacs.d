e950dd1d20e79485f7c16449cd600a7588706172
Oshiumi <toshiumi@aiming-inc.com>
2018-03-20 17:50:45 -0700

    save scratch to private/scratch/scratch and commit when killing emacs

--- auto-save.el
+++ auto-save.el
@@ -7,9 +7,14 @@
 ;;; !!!これを有効にするとauto-revertが効かなくなる!!!
 ;;; (setq auto-save-buffers-enhanced-quiet-save-p t)
 
-;;; *scratch*も ~/.emacs.d/scratch に自動保存
+;;; *scratch*も ~/.emacs.d/private/scratch/scratch に自動保存
 (setq auto-save-buffers-enhanced-save-scratch-buffer-to-file-p t)
 (setq auto-save-buffers-enhanced-file-related-with-scratch-buffer
-       (locate-user-emacs-file "scratch"))
+       (locate-user-emacs-file "private/scratch/scratch"))
 
 (auto-save-buffers-enhanced t)
+
+(defun commit-scratch ()
+  (shell-command "cd /Users/toshiumi/.emacs.d/private/scratch/ && git add scratch && git commit -m save"))
+
+(add-hook 'kill-emacs-hook 'commit-scratch)
