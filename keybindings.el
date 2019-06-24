2cb63100a7e4edbabb8b909a0eabd9de6a1584eb
Oshiumi <taichi.oshiumi.t.o@gmail.com>
2019-06-24 20:39:27 +0900

    Add multi-term keybinding

--- keybindings.el
+++ keybindings.el
@@ -5,4 +5,5 @@
 (keyboard-translate ?\C-h ?\C-?)
 (global-set-key "\C-H" 'backward-kill-word)
 (spacemacs/set-leader-keys
-  "o" 'evil-avy-goto-char-2)
+  "o" 'evil-avy-goto-char-2
+  "b t" 'multi-term)
