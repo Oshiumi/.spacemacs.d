bc8f5ad9b2654ef049c035093aad7cea2249b2b7
Oshiumi <taichi.oshiumi.t.o@gmail.com>
2019-06-24 20:38:43 +0900

    Fix error

--- init.el
+++ init.el
@@ -312,6 +312,7 @@ values."
    ;; delete only whitespace for changed lines or `nil' to disable cleanup.
    ;; (default nil)
    dotspacemacs-whitespace-cleanup nil
+   dotspacemacs-mode-line-theme 'spacemacs
    ))
 
 (defun dotspacemacs/user-init ()
