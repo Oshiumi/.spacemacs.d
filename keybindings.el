027c35c83717e3a1d8b13183fb4ab4b2f53eba0f
Oshiumi <taichi.oshiumi.t.o@gmail.com>
2020-02-25 13:09:25 +0900

    update

--- keybindings.el
+++ keybindings.el
@@ -5,5 +5,5 @@
 (keyboard-translate ?\C-h ?\C-?)
 (global-set-key "\C-H" 'backward-kill-word)
 (spacemacs/set-leader-keys
-  "o" 'evil-avy-goto-char-2
+  "o" 'evil-avy-goto-char-timer
   "b t" 'multi-term)
