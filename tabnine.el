ee917118bde182f2f381ef596da45b6e2c1068fc
Oshiumi <taichi.oshiumi.t.o@gmail.com>
2022-09-30 20:36:51 +0900

    Add tabnine

---
+++ tabnine.el
@@ -0,0 +1,8 @@
+(require 'company-tabnine)
+(add-to-list 'company-backends #'company-tabnine)
+
+;; Trigger completion immediately.
+(setq company-idle-delay 0)
+
+;; Number the candidates (use M-1, M-2 etc to select completions).
+(setq company-show-numbers t)
